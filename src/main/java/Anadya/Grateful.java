fa0aacd3fce7291cf59dc8275fa3b4b09d6e83dd
sunilnihalduggal <duggalyuvraj12@gmail.com>
2022-06-11 11:31:11 +0530

    andy

---
+++ src/main/java/Anadya/Grateful.java
@@ -0,0 +1,8 @@
+package Anadya;
+
+public class Grateful {
+
+    public static void grateful(String []args){
+        System.out.println("for everything you do everyday because theres nobody like you");
+    }
+}
---
+++ src/main/java/Anadya/everyday.java
@@ -0,0 +1,7 @@
+package Anadya;
+
+public class everyday {
+    public static void main(String []args){
+        System.out.println("thank you for making me feel everything to the fullest and more");
+    }
+}
