fee8aea4d1563a82bbf8178e9bfa7cc2e2fa3b16
Yuvraj Duggal <67824273+ynd24@users.noreply.github.com>
2022-07-02 18:27:23 +0530

    Delete src/main/java/Anadya directory

--- src/main/java/Anadya/Grateful.java
+++
@@ -1,8 +0,0 @@
-package Anadya;
-
-public class Grateful {
-
-    public static void grateful(String []args){
-        System.out.println("for everything you do everyday because theres nobody like you");
-    }
-}
--- src/main/java/Anadya/everyday.java
+++
@@ -1,7 +0,0 @@
-package Anadya;
-
-public class everyday {
-    public static void main(String []args){
-        System.out.println("thank you for making me feel everything to the fullest and more");
-    }
-}
